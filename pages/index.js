64ffada8d38e294e6a1b11d777829bc7bd5e95a5
Javiera Tapia <javierafrancisca92@gmail.com>
2018-12-13 02:01:19 -0300

    Route masking

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import Layout from '../components/MyLayout.js'
 import Link from 'next/link'
 
+/** Clean URLs with Route Masking
+Here, we are going to use a unique feature of Next.js called route masking. Basically, it will show a different URL on the browser than the actual URL that your app sees.
+*/
 const PostLink = (props) => (
   <li>
-    <Link href={`/post?title=${props.title}`}>
+    <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
       <a>{props.title}</a>
     </Link>
   </li>
@@ -13,9 +16,9 @@ export default () => (
   <Layout>
     <h1>My Blog</h1>
     <ul>
-      <PostLink title="Hello Next.js"/>
-      <PostLink title="Learn Next.js is awesome"/>
-      <PostLink title="Deploy apps with Zeit"/>
+      <PostLink id="hello-nextjs" title="Hello Next.js"/>
+      <PostLink id="learn-nextjs" title="Learn Next.js is awesome"/>
+      <PostLink id="deploy-nextjs" title="Deploy apps with Zeit"/>
     </ul>
   </Layout>
 )
